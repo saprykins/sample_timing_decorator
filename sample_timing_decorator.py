4c8c43b123211d57506b73dfe41847c49b826cc1
saprykins <palatatot@gmail.com>
2021-10-02 22:16:14 +0200

    output was improved

--- sample_timing_decorator.py
+++ sample_timing_decorator.py
@@ -1,24 +1,29 @@
 #! /usr/bin/env python
-import time
 
+import time
 def timer(function):
     # This is an example of decorator.
     # It calculates time of a function's cycle
     def wrapper():
         start_time = time.time()
-        func = function()
+        function()
         end_time = time.time()
-        return end_time - start_time
+        print(f"It took {end_time - start_time:.2f} seconds to run a function")
     return wrapper
+
 
 @timer
 def a_function():
     # This is a sample function that should be replaced by your function
     time.sleep(0.5)
-    print('something is happening')
+    print('a function does something')
+
+
+@timer
+def a_function():
+    # This is a sample function that should be replaced by your function
+    time.sleep(0.5)
+    print("a function does something")
 
 if __name__ == "__main__":
     print(a_function())
-
-
-    
